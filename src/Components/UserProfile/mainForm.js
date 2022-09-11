f2bbe3f4a97cdebd55a79769ea833ffa021635f1
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-09-11 14:11:53 -0700

    refactored the error messages

--- src/Components/UserProfile/mainForm.js
+++ src/Components/UserProfile/mainForm.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
-import { Form, Formik, ErrorMessage, useField } from "formik";
+import { Form, Formik, useField } from "formik";
 import { profileSchema } from "../../Schemas/index";
 
 const InputField = ({ ...props }) => {
   const [field, meta] = useField(props);
 
   return (
-    <input
-      className={`${
-        meta.touched && meta.error ? "invalid-input " : "valid-input "
-      }input`}
-      {...field}
-      {...props}
-    />
+    <>
+      <input
+        className={`${
+          meta.touched && meta.error ? "invalid-input " : "valid-input "
+        }input`}
+        {...field}
+        {...props}
+      />
+      {meta.error && meta.touched && (
+        <p className="inp-err-mssg">{meta.error}</p>
+      )}
+    </>
   );
 };
 
@@ -36,38 +41,20 @@ const MainForm = () => {
         className="flex flex-col h-screen justify-center items-center"
       >
         <InputField name="name" type="text" placeholder="Name" />
-        <ErrorMessage name="name" component="p" className="inp-err-mssg" />
         <InputField name="birthday" type="date" />
-        <ErrorMessage name="birthday" component="p" className="inp-err-mssg" />
         <div>
           <label>
             <InputField name="gender" type="radio" value="female" /> Female
-            <ErrorMessage
-              name="gender"
-              component="p"
-              className="inp-err-mssg"
-            />
           </label>
           <label>
             <InputField name="gender" type="radio" value="male" /> Male
-            <ErrorMessage
-              name="gender"
-              component="p"
-              className="inp-err-mssg"
-            />
           </label>
           <label>
             <InputField name="gender" type="radio" value="Non-binary" />{" "}
             Non-binary
-            <ErrorMessage
-              name="gender"
-              component="p"
-              className="inp-err-mssg"
-            />
           </label>
         </div>
         <InputField name="pic" type="file" />
-        <ErrorMessage name="pic" component="p" className="inp-err-mssg" />
       </Form>
     </Formik>
   );
