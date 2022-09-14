acbdc2d6eeb2feeab07da40519df0b2f2217de5d
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-09-13 19:32:13 -0700

    added image validation rules

--- src/Schemas/index.js
+++ src/Schemas/index.js
@@ -1,6 +1,8 @@
 import * as yup from "yup";
 import dayjs from "dayjs";
 
+const SUPPORTED_FILE_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
+
 export const signupSchema = yup.object().shape({
   email: yup
     .string()
@@ -49,15 +51,12 @@ export const signinSchema = yup.object().shape({
     .required("Required"),
 });
 
-export const nameSchema = yup.object().shape({
+export const nameAndBirthdaySchema = yup.object().shape({
   name: yup
     .string()
     .min(2, "Name must be at least 2 characters long")
     .max(22)
     .required("Required"),
-});
-
-export const birthdaySchema = yup.object().shape({
   birthday: yup
     .string()
     .required("Required")
@@ -71,5 +70,14 @@ export const genderSchema = yup.object().shape({
 });
 
 export const picSchema = yup.object().shape({
-  pic: yup.string().required("Required"),
+  pic: yup
+    .mixed()
+    .required("Required")
+    .test(
+      "FILE_FORMAT",
+      "Only jpg/jpeg, png image files are allowed",
+      (value) => {
+        return value && SUPPORTED_FILE_FORMATS.includes(value.type);
+      }
+    ),
 });
