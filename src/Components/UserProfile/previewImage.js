6229a7266fc79eb5bd0375cc2b108995974c060f
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-09-19 15:43:21 -0700

    added modal functionality for editing the uploaded image

--- src/Components/UserProfile/previewImage.js
+++ src/Components/UserProfile/previewImage.js
@@ -4,14 +4,9 @@ import { AiOutlineClose } from "react-icons/ai";
 import { AiOutlineCheck } from "react-icons/ai";
 import "cropperjs/dist/cropper.css";
 
-const PreviewImage = ({ imgurl, setModalOn, setFieldValue }) => {
-  const [croppedImg, setCroppedImg] = useState(null);
+const PreviewImage = ({ img, setModalOn, setFieldValue }) => {
   const cropperRef = useRef(null);
   const [zoomVal, setZoomVal] = useState(0);
-
-  useEffect(() => {
-    setZoomVal(0);
-  }, [imgurl]);
 
   const handleZoomChange = (e) => {
     setZoomVal((prevZoom) => {
@@ -29,67 +24,61 @@ const PreviewImage = ({ imgurl, setModalOn, setFieldValue }) => {
   const handleChoose = () => {
     const imgEle = cropperRef.current;
     const cropper = imgEle.cropper;
-    cropper
-      .crop()
-      .getCroppedCanvas()
-      .toBlob((blob) => {
-        const imgUrl = URL.createObjectURL(blob);
-        console.log(imgUrl);
-        setFieldValue("pic", imgUrl);
-      });
+    const imgUrl = {
+      url: cropper
+        .setCropBoxData({ left: 0, top: 0, width: 288, height: 240 })
+        .getCroppedCanvas()
+        .toDataURL(img.type, 0.5),
+      type: "image/jpeg",
+    };
+    setFieldValue("pic", imgUrl);
     setModalOn(false);
   };
 
   const handleCancel = () => {
+    setFieldValue("pic", null);
     setModalOn(false);
   };
 
   return (
-    <div className="bg-zinc-200 opacity-100 fixed inset-0 z-50">
-      <div className="flex h-screen justify-center items-center">
-        <div className="flex-col justify-center  bg-white py-12 px-24 border-4 border-sky-500 rounded-xl ">
-          <div className="flex">
-            <button onClick={handleCancel}>
-              <AiOutlineClose />
-            </button>
-            <button>
-              <AiOutlineCheck onClick={handleChoose} />
-            </button>
-          </div>
+    <div className="bg-white opacity-100 fixed inset-0 z-50">
+      <div className="flex flex-col h-screen justify-center items-center">
+        <div className="flex">
+          <button type="button" onClick={handleCancel}>
+            <AiOutlineClose />
+          </button>
+          <button type="button" onClick={handleChoose}>
+            <AiOutlineCheck />
+          </button>
+        </div>
+        <div className="image-section">
           <Cropper
             ref={cropperRef}
-            src={imgurl}
+            src={img.url}
             dragMode="move"
-            preview=".img-preview"
-            style={{ height: "288px", width: "100%" }}
-            aspectRatio={16 / 9}
-            viewMode={3}
+            preview={".img-preview"}
+            viewMode={0}
             background={true}
-            autoCrop={false}
+            autoCrop={true}
+            autoCropArea={1}
             cropBoxMovable={false}
             cropBoxResizable={false}
-            minContainerHeight={288}
-            minContainerWidth={288}
-            minCanvasWidth={288}
-            minCanvasHeight={288}
             toggleDragModeOnDblclick={false}
+            zoomOnTouch={false}
+            zoomOnWheel={false}
           />
-          <input
-            value={zoomVal}
-            type="range"
-            min="0"
-            max="1"
-            step="0.02"
-            className="slider absolute top-[17.2rem] lg:top-[21rem]"
-            onChange={handleZoomChange}
-            id="myRange"
-          />
-
-          <div
-            className="img-preview"
-            style={{ width: "100%", float: "left", height: "300px" }}
-          ></div>
+          <div className="img-preview"></div>
         </div>
+        <input
+          value={zoomVal}
+          type="range"
+          min="-0.3"
+          max="1"
+          step="0.02"
+          className="slider"
+          onChange={handleZoomChange}
+          id="myRange"
+        />
       </div>
     </div>
   );
