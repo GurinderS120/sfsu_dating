882f10693eee39c38ba30bb2df07d8742e8f28e5
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-09-29 11:52:05 -0700

    increased the size of checkbox on large screens

--- src/Components/FormikComponents/CheckBoxField.tsx
+++ src/Components/FormikComponents/CheckBoxField.tsx
@@ -6,9 +6,14 @@ const CheckBoxField = (props: FieldHookConfig<string>) => {
   const { name, type } = props;
 
   return (
-    <label className="flex flex-row justify-between mb-2 max-w-[15rem] lg:max-w-[16rem]">
+    <label className="flex flex-row justify-between mb-2 max-w-[14rem] lg:max-w-[16rem]">
       {props.value}
-      <input className="w-3 ml-16" {...field} name={name} type={type} />
+      <input
+        className="w-3 lg:scale-125 ml-16"
+        {...field}
+        name={name}
+        type={type}
+      />
     </label>
   );
 };
