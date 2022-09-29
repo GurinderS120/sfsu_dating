330f3599f95947566ad9af1e3aba43fc54ef134d
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-09-29 10:04:55 -0700

    created an additional file to store list values that can be used for inputs

---
+++ src/Components/UserProfile/Data.tsx
@@ -0,0 +1,17 @@
+// We will use this file to store list values that can be used in various inputs
+// like radio buttons, select, checkboxes
+
+export const activitiesArr = [
+  "Studying",
+  "Playing video games",
+  "Working",
+  "Having fun with friends and family",
+  "Playing an outdoor sport",
+  "Exercising",
+  "Watching movies and tv shows",
+  "Going for a walk",
+  "Going for a long drive",
+  "Trying new food",
+  "Shopping",
+  "Doing nothing",
+];
